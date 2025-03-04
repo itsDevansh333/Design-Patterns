590b0da6ca1d70615207813d5d310b3e5984a39c
Devansh <131320569+itsDevansh333@users.noreply.github.com>
2025-03-04 14:10:05 +0530

    Create AndroidDeveloper.java

---
+++ FactoryDesign/AndroidDeveloper.java
@@ -0,0 +1,12 @@
+package FactoryPattern;
+
+public class AndroidDeveloper implements Employee {
+
+	@Override
+	public int getSalary() {
+		// TODO Auto-generated method stub
+		System.out.println("Getting Android Developer Salary");
+		return 50000;
+	}
+
+}
