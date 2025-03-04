de7659ae105ca9e1e0c8a58dbb5d3e793cd83516
Devansh <131320569+itsDevansh333@users.noreply.github.com>
2025-03-04 14:11:24 +0530

    Create WebDeveloper.java

---
+++ FactoryDesign/WebDeveloper.java
@@ -0,0 +1,14 @@
+package FactoryPattern;
+
+public class WebDevleoper implements Employee {
+
+	@Override
+	public int getSalary() {
+		// TODO Auto-generated method stub
+		System.out.println("Getting web developer salary");
+		return 40000;
+		
+	}
+
+}
+
