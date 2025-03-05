0dc51e2c2268515e4c80f0389d121da47143c2f4
Devansh <131320569+itsDevansh333@users.noreply.github.com>
2025-03-05 14:11:46 +0530

    Update Employee.java

--- AbstractFactoryPattern/Employee.java
+++ AbstractFactoryPattern/Employee.java
@@ -1,14 +1,6 @@
 package AbstractFactoryPattern;
 
-public class client {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			// getting android developer
-		Employee e1=EmployeeFactory.getEmployee(new AndroidDeveloperFactory());
-		e1.name();
-		Employee e2=EmployeeFactory.getEmployee(new WebDeveloperFactory());
-		e2.name();
-	}
-
+public interface Employee {
+	int salary();
+	String name();
 }
