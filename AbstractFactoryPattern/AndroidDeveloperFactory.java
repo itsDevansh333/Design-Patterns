04893db0ae1107fc2da8eab94837e093a1680394
Devansh <131320569+itsDevansh333@users.noreply.github.com>
2025-03-05 14:13:59 +0530

    Create AndroidDeveloperFactory.java

---
+++ AbstractFactoryPattern/AndroidDeveloperFactory.java
@@ -0,0 +1,11 @@
+package AbstractFactoryPattern;
+
+public class AndroidDeveloperFactory extends EmployeeAbstractFactory {
+
+	@Override
+	public Employee createEmployee() {
+		// TODO Auto-generated method stub
+		return new AndroidDeveloper();
+	}
+
+}
